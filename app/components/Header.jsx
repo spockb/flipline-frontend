967f17f7519252dfa88fadabe75fb56620508e81
spockb <178428622+spockb@users.noreply.github.com>
2025-08-05 15:33:59 -0700

    Added mantine ui

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,3 +1,29 @@
+import { NavLink, Link } from "react-router-dom";
 export default function Header() {
-  return <div>Header</div>;
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+  return (
+    <>
+      <header>
+        <Link to="/">InvestoPresto</Link>
+        <nav>
+          <NavLink
+            to="/listings"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Listings
+          </NavLink>
+          <NavLink
+            to="/about"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            About
+          </NavLink>
+        </nav>
+      </header>
+    </>
+  );
 }
